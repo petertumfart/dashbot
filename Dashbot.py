6628ef4b6255bb095f90572cd498d23841d3de47
Peter Tumfart <peter.tumfart@meon-medical.com>
2021-10-18 19:40:58 +0200

    Changed title

--- Dashbot.py
+++ Dashbot.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.title('Cryptobot')
+st.title('Dashbot')
 
 st.write('Stay tuned...')
 st.write('Shoutouts to: [Flothohub](https://github.com/FloThoHub) and [geilertyp.at](http://geilertyp.at)')
