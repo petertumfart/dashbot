53f2133557f14dbbfafe1e90b49b6e81150b477d
Peter Tumfart <peter.tumfart@gmx.at>
2021-10-18 22:28:53 +0200

    Catching unrecognized mail error

--- utils.py
+++ utils.py
@@ -2,9 +2,12 @@ import logon_data
 
 
 def extract_combination(mail, pwd):
-    idx = logon_data.mail_data.index(mail)
-    if pwd == logon_data.pwd_data[idx]:
-        return True
+    try:
+        idx = logon_data.mail_data.index(mail)
+        if pwd == logon_data.pwd_data[idx]:
+            return True
+    except:
+        return False
 
     return False
 
