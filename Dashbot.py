dd0272335a6f704e4eda2dee7b2d6c38cd831f11
Peter Tumfart <peter.tumfart@meon-medical.com>
2021-10-18 19:38:20 +0200

    Basic example

---
+++ Dashbot.py
@@ -0,0 +1,6 @@
+import streamlit as st
+
+st.title('Cryptobot')
+
+st.write('Stay tuned...')
+st.write('Shoutouts to: [Flothohub](https://github.com/FloThoHub) and [geilertyp.at](http://geilertyp.at)')
